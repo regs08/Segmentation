7acb9d1596127f3d345502f055e408e8cd023577
cregs08 <cole.regnier@gmail.com>
2022-11-20 19:39:32 -0800

    completd the TODO of inheriting from the gen from file list class.

--- Preprocessing/Generators/patchify_gen.py
+++ Preprocessing/Generators/patchify_gen.py
@@ -16,31 +16,25 @@ Shout out to github account bnsreenu, the function patchify_and_save_image_mask_
 here: https://github.com/bnsreenu/python_for_microscopists/blob/master/219_unet_small_dataset_using_functional_blocks.py
 """
 import os
-import glob
 import cv2
 import numpy as np
 from patchify import patchify
+from Segmentation.Preprocessing.Generators.gen_from_file_list import GenFromFileList
 
-
-class PatchifyGen():
-
+class PatchGen(GenFromFileList):
     def __init__(self,
-                image_mask_dir,
-                save_dir,
-                batch_size=8,
-                image_exts=['.jpg', '.tiff', '.png', '.jpeg'],
-                save_ext='.png',
-                patch_height=512,
-                patch_width=512,
-                step=512,
-                resize=False,
-                resize_height=256,
-                resize_width=256,
-                ):
-        self.image_mask_dir = image_mask_dir
+                 image_dir,
+                 save_dir,
+                 save_ext='.png',
+                 patch_height=512,
+                 patch_width=512,
+                 step=512,
+                 resize=False,
+                 resize_height=256,
+                 resize_width=256,
+                 ):
+        super().__init__(image_dir=image_dir)
         self.save_dir = save_dir
-        self.batch_size = batch_size
-        self.image_exts = image_exts
         self.save_ext = save_ext
         self.patch_height=patch_height
         self.patch_width=patch_width
@@ -48,20 +42,10 @@ class PatchifyGen():
         self.resize_height = resize_height
         self.resize_width = resize_width
         self.step = step
-        self.file_list = self.get_image_mask_files()
+
+        #call from super
+        self.file_list = self.get_image_files()
         self.batch_gen = self.get_batch_gen_from_file_list()
-
-    def get_batch_gen_from_file_list(self):
-
-        num_files = len(self.file_list)
-        steps = num_files // self.batch_size
-        remainder = num_files % self.batch_size
-        for i in range(0, steps):
-            lesser_idx = i * self.batch_size
-            greater_idx = (i + 1) * self.batch_size
-
-            yield self.file_list[lesser_idx:greater_idx]
-        yield self.file_list[num_files - remainder:num_files]
 
     def patchify_and_save_from_all_data(self):
         while True:
@@ -84,7 +68,7 @@ class PatchifyGen():
         img_arr = self.load_image(image_mask_path)
         patches = patchify(img_arr, (self.patch_height, self.patch_width, 3), step=self.step)
         out_patches = []
-        patch_num = 0
+        num_patches = 0
         for i in range(patches.shape[0]):
             for j in range(patches.shape[1]):
                 single_patch = patches[i, j, :, :]
@@ -93,11 +77,12 @@ class PatchifyGen():
                 if self.resize:
                     single_patch = cv2.resize(single_patch, (self.resize_height, self.resize_width))
 
-                patch_save_path = self.get_patch_save_name(image_mask_path, patch_num)
-                assert cv2.imwrite(patch_save_path, single_patch), print(f'Saved failed for {patch_save_path}')
+
+                patch_save_path = self.get_patch_save_name(image_mask_path, num_patches)
+                assert cv2.imwrite(patch_save_path, cv2.cvtColor(single_patch, cv2.COLOR_BGR2RGB)), print(f'Saved failed for {patch_save_path}')
                 out_patches.append(single_patch)
-                patch_num += 1
-        print(f'Saved {patch_num} patches to {self.save_dir}!!')
+                num_patches += 1
+        print(f'Saved {num_patches} patches to {self.save_dir}!!')
 
         return np.array(out_patches)
 
@@ -107,26 +92,19 @@ class PatchifyGen():
         patch_save_path = os.path.join(self.save_dir, patch_name)
         return patch_save_path
 
-    def load_image(self, image_path):
-        image = cv2.imread(image_path, 1)
-        # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        return image
 
-    def get_image_mask_files(self):
-
-        file_list = []
-        for ext in self.image_exts:
-            glob_path = os.path.join(self.image_mask_dir, '*' + ext)
-            file_list.extend(glob.glob(glob_path))
-        return file_list
-
-
-class HeightWisePatchifyGen(PatchifyGen):
+class HeightWisePatchifyGen(PatchGen):
     """
     Only modification to the original method is that we set the patch height to the height of the image
     """
+    ##
+    #overridding method
+    ##
     def patchify_and_save_image_mask_file(self, image_mask_path):
         img_arr = self.load_image(image_mask_path)
+        ####
+        #setting our patch height to the height of the image
+        ####
         patch_height = img_arr.shape[0]
         patches = patchify(img_arr, (patch_height, self.patch_width, 3), step=self.step)
         out_patches = []
@@ -143,17 +121,30 @@ class HeightWisePatchifyGen(PatchifyGen):
                 assert cv2.imwrite(patch_save_path, single_patch), print(f'Saved failed for {patch_save_path}')
                 out_patches.append(single_patch)
                 patch_num += 1
+
         print(f'Saved {patch_num} patches to {self.save_dir}!!')
 
         return np.array(out_patches)
+
+
+
+
 image_dir = "/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/Images/Pinot-Noir"
 image_patch_dir = "/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/patchify/images/Pinot-Noir"
 
 mask_dir = "/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/Masks/Pinot-Noir"
 mask_patch_dir = "/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/patchify/masks/Pinot-Noir"
 
-height_wise_images = HeightWisePatchifyGen(image_dir, image_patch_dir)
-height_wise_images.patchify_and_save_from_all_data()
+#
+# height_wise_images = HeightWisePatchifyGen(image_dir, image_patch_dir, resize_height=1024, resize_width=1024, resize=True)
+# height_wise_images.patchify_and_save_from_all_data()
+#
+# height_wise_masks = HeightWisePatchifyGen(mask_dir, mask_patch_dir,resize_height=1024, resize_width=1024, resize=True)
+# height_wise_masks.patchify_and_save_from_all_data()
 
-height_wise_masks = HeightWisePatchifyGen(mask_dir, mask_patch_dir)
-height_wise_masks.patchify_and_save_from_all_data()
+img=cv2.imread("/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/patchify/images/Pinot-Noir/IMG_0199_patch_2.png")
+img  = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('',img)
+cv2.waitKey()
+
+

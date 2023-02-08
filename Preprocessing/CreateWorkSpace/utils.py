a1f03922964c8c77e6e2d13f31904482511d1195
cregs08 <cole.regnier@gmail.com>
2023-02-08 12:04:04 -0300

    finishing up create workspace

--- Preprocessing/CreateWorkSpace/utils.py
+++ Preprocessing/CreateWorkSpace/utils.py
@@ -41,4 +41,34 @@ project name
 
 
 """
+import os
 
+
+def make_dir_if_none(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+    else:
+        print(f'Dir {d} already exists skipping...')
+
+
+def create_workspace(root_dir, model_name, default_training_layers=['heads', '4+', 'all']):
+
+    out_dir = os.path.join(root_dir, "Out")
+    model_dir = os.path.join(out_dir, "Models")
+
+    model_name_dir = os.path.join(model_dir, model_name)
+
+    DIRS = {
+        'PROJECT': root_dir,
+        'OUT': out_dir,
+        'MODEL': model_dir,
+        'MODEL_NAME': model_name_dir,
+    }
+
+    for layer in default_training_layers:
+        DIRS[f'{model_name}_{layer}'] = os.path.join(model_name_dir, layer)
+
+    for d in DIRS:
+        make_dir_if_none(DIRS[d])
+
+    return DIRS

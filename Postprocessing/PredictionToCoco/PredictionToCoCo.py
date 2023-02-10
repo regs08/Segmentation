6b0d4a42eb38bb92631b233ff45212939b41ce28
cregs08 <cole.regnier@gmail.com>
2023-02-10 18:05:44 -0300

    gettign rid of some local files as well as creating a new method

--- Postprocessing/PredictionToCoco/PredictionToCoCo.py
+++ Postprocessing/PredictionToCoco/PredictionToCoCo.py
@@ -6,17 +6,31 @@ from Segmentation.Postprocessing.PredictionToCoco.utils import create_prediction
 
 import os
 
-weight_path = "/Users/cole/PycharmProjects/Forgit/Segmentation/Model/Weights/grape_aug_patchify.h5"
-image_path = ["/Users/cole/PycharmProjects/Forgit/Image_Files/Patchify/Images/IMG_0199_patch_2.png"]
-cat = dict()
-cat['grape'] = 1
+
+def get_image_anns_seg_anns_from_prediction(pred, filename, image_id):
+    """
+    takes in a single prediction from model.detect
+    returns the image and segmentation anns in a coco format
+    :param pred: prediction from mrcnn model.detect
+    :param image_id: image_id to be assigned to the prediction
+    :return: list: images, list: anns
+    """
+
+    if not pred['masks'].any():
+        print(f"NO PREDICTION FOUND FOR {filename}")
+
+    h, w, _ = pred['masks'].shape
+
+    images = (create_image_annotation(filename, height=h, width=w, image_id=image_id))
+    anns = create_prediction_submask_anns(pred, image_id=image_id)
+
+    return images, anns
 
 
-def prediction_to_coco(weight_path, image_paths, cat_dict, plot_json=False):
+def prediction_to_coco_from_image_paths(weight_path, image_paths, cat_dict, plot_json=False):
     model = define_prediction_model(weight_path)
     coco_format = get_coco_json_format()
     coco_format['categories'] = create_category_annotation(cat_dict)
-
     all_anns = []
     images = []
 
@@ -25,24 +39,12 @@ def prediction_to_coco(weight_path, image_paths, cat_dict, plot_json=False):
 
         pred = predict_on_image(model=model,
                                   image_path=image_path)
-        if not pred['masks'].any():
-            print(f"NO PREDICTION FOUND FOR {filename}")
+        img, anns = get_image_anns_seg_anns_from_prediction(pred, filename, image_id=i)
 
-        h, w, _= pred['masks'].shape
-
-        images.append(create_image_annotation(filename, height=h, width=w, image_id=i))
-        all_anns.extend(create_prediction_submask_anns(pred, image_id=i))
-
+        images.append(img)
+        anns.extend(anns)
     coco_format['images'] = images
     coco_format['annotations'] = all_anns
 
     return coco_format
 
-outfile = "/Users/cole/PycharmProjects/Forgit/Segmentation/tesing_dir/conversionTests/predToJson/predToCocoTest.json"
-
-coco_format = prediction_to_coco(weight_path, image_path, cat)
-save_json(outfile, coco_format)
-
-
-image_dir = "/Users/cole/PycharmProjects/Forgit/Image_Files/Patchify/Images"
-plot_from_json(json_file=outfile, image_dir=image_dir)

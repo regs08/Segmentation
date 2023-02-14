6fe3157c68f0f6ec2e6aa8e6873c6e87563bd623
cregs08 <cole.regnier@gmail.com>
2023-02-14 15:57:12 -0300

    making it a bit more flexible commented out some of the wrapper functions

--- Preprocessing/CheckImageMasks/CheckNumInstances.py
+++ Preprocessing/CheckImageMasks/CheckNumInstances.py
@@ -8,55 +8,18 @@ import glob
 import shutil
 
 from PIL import Image
-from collections import defaultdict
+import re
+import numpy as np
 
 
-def check_masks_for_min_masks_and_remove_replace(min_instances, out_dir, orig_image_mask_dir):
+def get_num_masks(mask_path):
     """
-    checks our masks for the min number of masks. if they don't have the min number they are deleted and replaced with
-    the orginal image.
-    :param min_instances:
-    :param out_dir: where our patches our stored. we check these
-    :param orig_image_mask_dir: original images. replace from these
-    :return:
-    """
-    orig_image_dir = os.path.join(orig_image_mask_dir, 'Images')
-    orig_mask_dir = os.path.join(orig_image_mask_dir, 'Masks')
-
-    patch_image_dir = os.path.join(out_dir, 'Images')
-    patch_mask_dir = os.path.join(out_dir, 'Masks')
-
-    glob_path = os.path.join(patch_mask_dir, '*')
-    mask_paths = glob.glob(glob_path)
-
-    masks_to_be_removed = get_masks_to_be_removed(mask_paths, min_instances)
-    if masks_to_be_removed:
-        remove_from_patch_dir_and_copy_from_orig_dir(masks_to_be_removed,
-                                                     patch_image_dir=patch_image_dir,
-                                                     patch_mask_dir=patch_mask_dir,
-                                                     orig_image_dir=orig_image_dir,
-                                                     orig_mask_dir=orig_mask_dir)
-
-
-def get_num_masks(mask_path, min_instances):
-    """
-    checks for number of masks by counting the number of unique pixels.
+    checks for number of masks by counting the number of unique pixels
     :param mask_path: mask were checking for unique masks
-    :param min_instances: number of min masks per patched image
-    :return: original filenames used to copy the whole image over to our image and mask dir
+    :return: num masks
     """
-    orig_filename = ''
-    im = Image.open(mask_path)
-    by_color = defaultdict(int)
-    for pixel in im.getdata():
-        by_color[pixel] += 1
-    colors = list(by_color.keys())
-    num_masks = len(colors) - 1  #-1 for background
-    print(num_masks)
-    if num_masks < min_instances:
-        filename = os.path.basename(mask_path)
-        orig_filename = filename.split('_patch')[0]
-    return orig_filename
+    im = np.asarray(Image.open(mask_path))
+    return len(np.unique(im.reshape(-1, im.shape[2]), axis=0))
 
 
 def get_masks_to_be_removed(mask_paths, min_instances):
@@ -68,9 +31,12 @@ def get_masks_to_be_removed(mask_paths, min_instances):
     """
     masks_to_be_removed = []
     for mask_path in mask_paths:
-        low_instance_mask_basename = get_num_masks(mask_path, min_instances)
-        if low_instance_mask_basename:
-            masks_to_be_removed.append(low_instance_mask_basename)
+
+        num_masks = get_num_masks(mask_path)
+
+        if num_masks < min_instances:
+            masks_to_be_removed.append(os.path.basename(mask_path))
+
     return masks_to_be_removed
 
 
@@ -111,40 +77,72 @@ def find_and_remove_images_from_dir(filenames, search_dir):
     return filenames
 
 
-def copy_image_to_dir(orig_filenames, source_dir, dest_dir):
-    """
-    copies the orignal file, the unpatchified one to the current training dir
-    :param orig_filenames:
-    :param source_dir:
-    :param dest_dir:
-    :return:
-    """
-    for filename in orig_filenames:
-        filename_with_ext = glob.glob(get_glob_path_for_filename(source_dir, filename))
-        assert len(filename_with_ext) == 1, print(f'More than one or no instance of file: {filename}'
-                                                   f' found in source dir\n{source_dir}')
 
-        src = os.path.join(source_dir, filename_with_ext[0])
-        print(f'Copying, {filename_with_ext[0]} to {dest_dir}')
-        shutil.copy(src, dest_dir)
+"""
+Not being used 
+"""
 
-
-def remove_from_patch_dir_and_copy_from_orig_dir(masks_to_be_removed,
-                                                 patch_image_dir,
-                                                 patch_mask_dir,
-                                                 orig_image_dir,
-                                                 orig_mask_dir):
-    """
-    wrapper function for the find and remove, and copy functions
-    :param masks_to_be_removed:
-    :param patch_image_dir:
-    :param patch_mask_dir:
-    :param orig_image_dir:
-    :param orig_mask_dir:
-    :return:
-    """
-    find_and_remove_images_from_dir(masks_to_be_removed, patch_image_dir)
-    find_and_remove_images_from_dir(masks_to_be_removed, patch_mask_dir)
-
-    copy_image_to_dir(masks_to_be_removed, orig_image_dir, patch_image_dir)
-    copy_image_to_dir(masks_to_be_removed, orig_mask_dir, patch_mask_dir)
+# def check_masks_for_min_masks_and_remove_replace(min_instances, out_dir, orig_image_mask_dir):
+#     """
+#     checks our masks for the min number of masks. if they don't have the min number they are deleted and replaced with
+#     the orginal image.
+#     :param min_instances:
+#     :param out_dir: where our patches our stored. we check these
+#     :param orig_image_mask_dir: original images. replace from these
+#     :return:
+#     """
+#     orig_image_dir = os.path.join(orig_image_mask_dir, 'Images')
+#     orig_mask_dir = os.path.join(orig_image_mask_dir, 'Masks')
+#
+#     patch_image_dir = os.path.join(out_dir, 'Images')
+#     patch_mask_dir = os.path.join(out_dir, 'Masks')
+#
+#     glob_path = os.path.join(patch_mask_dir, '*')
+#     mask_paths = glob.glob(glob_path)
+#
+#     masks_to_be_removed = get_masks_to_be_removed(mask_paths, min_instances)
+#     if masks_to_be_removed:
+#         remove_from_patch_dir_and_copy_from_orig_dir(masks_to_be_removed,
+#                                                      patch_image_dir=patch_image_dir,
+#                                                      patch_mask_dir=patch_mask_dir,
+#                                                      orig_image_dir=orig_image_dir,
+#                                                      orig_mask_dir=orig_mask_dir)
+#
+#
+# def copy_image_to_dir(orig_filenames, source_dir, dest_dir):
+#     """
+#     copies the orignal file, the unpatchified one to the current training dir
+#     :param orig_filenames:
+#     :param source_dir:
+#     :param dest_dir:
+#     :return:
+#     """
+#     for filename in orig_filenames:
+#         filename_with_ext = glob.glob(get_glob_path_for_filename(source_dir, filename))
+#         assert len(filename_with_ext) == 1, print(f'More than one or no instance of file: {filename}'
+#                                                    f' found in source dir\n{source_dir}')
+#
+#         src = os.path.join(source_dir, filename_with_ext[0])
+#         print(f'Copying, {filename_with_ext[0]} to {dest_dir}')
+#         shutil.copy(src, dest_dir)
+#
+#
+# def remove_from_patch_dir_and_copy_from_orig_dir(masks_to_be_removed,
+#                                                  patch_image_dir,
+#                                                  patch_mask_dir,
+#                                                  orig_image_dir,
+#                                                  orig_mask_dir):
+#     """
+#     wrapper function for the find and remove, and copy functions
+#     :param masks_to_be_removed:
+#     :param patch_image_dir:
+#     :param patch_mask_dir:
+#     :param orig_image_dir:
+#     :param orig_mask_dir:
+#     :return:
+#     """
+#     find_and_remove_images_from_dir(masks_to_be_removed, patch_image_dir)
+#     find_and_remove_images_from_dir(masks_to_be_removed, patch_mask_dir)
+#
+#     copy_image_to_dir(masks_to_be_removed, orig_image_dir, patch_image_dir)
+#     copy_image_to_dir(masks_to_be_removed, orig_mask_dir, patch_mask_dir)
